d2976a0dce6a36e076e4305d3c9b8d7f53d3fc83
Kåre von Geijer <kare.kvg@gmail.com>
2024-07-02 16:54:09 +0200

    [WIP] implementing Spring system

--- src/spring_layout.rs
+++ src/spring_layout.rs
@@ -1,1 +1,49 @@
+use bevy::prelude::*;
 
+#[derive(Component)]
+struct Velocity {
+    value: Vec3,
+}
+
+impl Velocity {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Component)]
+struct Acceleration {
+    value: Vec3,
+}
+
+impl Acceleration {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Bundle)]
+struct MomentumBundle {
+    velocity: Velocity,
+    acceleration: Acceleration,
+}
+
+pub struct SpringLayoutPlugin;
+
+impl Plugin for SpringLayoutPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, (update_velocity, update_position));
+    }
+}
+
+fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>, time: Res<Time>) {
+    for (acceleration, mut velocity) in query.iter_mut() {
+        velocity.value += acceleration.value * time.delta_seconds();
+    }
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation += velocity.value * time.delta_seconds();
+    }
+}
